e87dbe6b4686fa07cf5c04f101c1f17fc02afb5c
Usuario <Usuario@DESKTOP-JRTC2U7>
2025-08-27 22:17:01 -0300

    fix: new changes in sort of files

--- src/main/java/com/globy/repository/TarefaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.globy.repository;
-
-import com.globy.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
